54bc3879b85cf55d7c900b626bb64c932550de81
JasonYang <jason53240@gmail.com>
2020-05-26 22:51:37 +0800

    demo

---
+++ demo/XBee_host.py
@@ -0,0 +1,71 @@
+import serial
+import time
+
+# XBee setting
+serdev = '/dev/ttyUSB0'
+s = serial.Serial(serdev, 9600)
+
+s.write("+++".encode())
+char = s.read(2)
+print("Enter AT mode.")
+print(char.decode())
+
+s.write("ATMY 0x148\r\n".encode())
+char = s.read(3)
+print("Set MY 0x148.")
+print(char.decode())
+
+s.write("ATDL 0x248\r\n".encode())
+char = s.read(3)
+print("Set DL 0x248.")
+print(char.decode())
+
+s.write("ATID 0x1\r\n".encode())
+char = s.read(3)
+print("Set PAN ID 0x1.")
+print(char.decode())
+
+s.write("ATWR\r\n".encode())
+char = s.read(3)
+print("Write config.")
+print(char.decode())
+
+s.write("ATMY\r\n".encode())
+char = s.read(4)
+print("MY :")
+print(char.decode())
+
+s.write("ATDL\r\n".encode())
+char = s.read(4)
+print("DL : ")
+print(char.decode())
+
+s.write("ATCN\r\n".encode())
+char = s.read(3)
+print("Exit AT mode.")
+print(char.decode())
+
+print("start sending RPC")
+# while True:
+s.write(bytes("\r", 'UTF-8'))
+# line=s.readline() # Read an echo string from K66F terminated with '\n' (pc.putc())
+# print(line)
+# line=s.readline() # Read an echo string from K66F terminated with '\n' (RPC reply)
+# print(line)
+time.sleep(1)
+
+s.write("/getAcc/run\r".encode())
+# line=s.readline() # Read an echo string from K66F terminated with '\n' (pc.putc())
+# print(line.decode())
+# line=s.readline() # Read an echo string from K66F terminated with '\n' (RPC reply)
+# print(line.decode())
+time.sleep(1)
+
+s.write("/getAddr/run\r".encode())
+# line=s.readline() # Read an echo string from K66F terminated with '\n' (pc.putc())
+# print(line)
+# line=s.readline() # Read an echo string from K66F terminated with '\n' (RPC reply)
+# print(line)
+time.sleep(1)
+
+s.close()
